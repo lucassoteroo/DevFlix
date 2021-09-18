d493e4aeef902e098e563396d948a0ad6b5080e7
lucassoteroo <lucassoteroo14@gmail.com>
2021-09-18 10:37:20 -0300

    adicionando validação com JQuery

--- js/custom-login.js
+++ js/custom-login.js
@@ -1,4 +1,11 @@
 $('#entrar').click(function() {
-    window.location.href = './index.html';
-    return false;
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    if (email == "lucas_sotero@gmail.com" && senha == "123456789") {
+        window.location.href = './index.html';
+        return false;
+    } else {
+        alert("Email ou senha inválidos");
+        return false;
+    }
 });
