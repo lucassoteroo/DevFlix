e23c3c763dca6b22d694965836189716f798f6df
lucassoteroo <lucassoteroo14@gmail.com>
2021-10-15 21:40:15 -0300

    adicionando eventos e html

--- js/custom-login.js
+++ js/custom-login.js
@@ -1,24 +1,53 @@
 $(document).ready(function() {
 
-    $('#enviar').dblclick(function() {
-        const emailCadastrado = $('#cadastroEmail').val();
-        const senhaCadastrada = $('#cadastroSenha').val();
+    function validacao(elemento) {
+        if (elemento.val() == '') {
+            elemento.addClass('invalido');
+            return false;
+        } else {
+            elemento.removeClass('invalido');
+        }
+    }
 
-        $('.container-cadastro').addClass('visually-hidden');
-        $('.container-login').removeClass('visually-hidden');
-
-        $('#entrar').click(function() {
-            let email = $('#email').val();
-            let senha = $('#senha').val();
-            if (email == emailCadastrado && senha == senhaCadastrada) {
-                window.location.href = './index.html';
-                return false;
-            } else {
-                alert("Email ou senha inválidos");
-                return false;
-            }
-        });
+    $('#cadastroEmail').on('blur', function() {
+        validacao($(this));
     })
+    $('#cadastroSenha').on('blur', function() {
+        validacao($(this));
+    })
+
+    $('#enviar').dblclick(function(e) {
+        e.preventDefault();
+        const emailCadastrado = $('#cadastroEmail');
+        const senhaCadastrada = $('#cadastroSenha');
+        const NomeUsuario = $('#nome').val();
+
+        if (emailCadastrado.hasClass('invalido') || senhaCadastrada.hasClass('invalido') || emailCadastrado.val() == '' || senhaCadastrada.val() == '') {
+            alert("Verifique os campos obrigatórios");
+            return false;
+        } else {
+            $('.container-cadastro').addClass('visually-hidden');
+            $('.container-login').removeClass('visually-hidden');
+            $('form').prepend('<h3>Olá ' + NomeUsuario + '</h3')
+
+            console.log(emailCadastrado);
+            console.log(senhaCadastrada);
+
+            $('#entrar').dblclick(function() {
+                let email = $('#email').val();
+                let senha = $('#senha').val();
+                if (email == '' || senha == '' || email != emailCadastrado.val() || senha != senhaCadastrada.val()) {
+                    $('#email').addClass('invalido');
+                    $('#senha').addClass('invalido');
+                    return false;
+                } else if (email == emailCadastrado.val() && senha == senhaCadastrada.val()) {
+                    window.location.href = './index.html';
+                    return true;
+                }
+            });
+        }
+    })
+
 
 
 });
